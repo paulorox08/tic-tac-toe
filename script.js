d3d0aac3ab4399aa69e7b9de0252e02fe35ef525
Paulo Adeva <paulo.adeva@gmail.com>
2022-05-03 23:33:29 +1000

    testing stuff

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-function Player(name) {
-    let player = [];
-    this.name = name;
-}
-
-// function trackPlayer() {
-//     let turn = [];
-//     let board = document.querySelector(".toprow > child, .middlerow > child, .bottomrow > child");;
+// function Player(name) {
+//     let player = [];
+//     this.name = name;
 // }
 
-// function move() {
-//     const 
-// }
+// // function trackPlayer() {
+// //     let turn = [];
+// //     let board = document.querySelector(".toprow > child, .middlerow > child, .bottomrow > child");;
+// // }
 
-const board = document.querySelectorAll(".toprow > child, .middlerow > child, .bottomrow > child");
-board.addEventListener('click', () => {
-    console.log("hello");
-});
+// // function move() {
+// //     const 
+// // }
+
+// const board = document.querySelectorAll(".toprow > child, .middlerow > child, .bottomrow > child");
+// board.addEventListener('click', () => {
+//     console.log("hello");
+// });
+
+
+const topRight = document.querySelector(".topright");
+const exes = document.createElement("div");
+exes.textContent = "X";
+topRight.appendChild(exes);
+
