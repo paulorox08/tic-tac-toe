4dbb611c0e8d3e8ceaf6c9f17e3aac10686ff629
Paulo Adeva <paulo.adeva@gmail.com>
2022-05-03 18:32:14 +1000

    there's bugs

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ function Player(name) {
     this.name = name;
 }
 
-function trackPlayer() {
-    let turn = [];
+// function trackPlayer() {
+//     let turn = [];
+//     let board = document.querySelector(".toprow > child, .middlerow > child, .bottomrow > child");;
+// }
 
+// function move() {
+//     const 
+// }
 
-}
+const board = document.querySelectorAll(".toprow > child, .middlerow > child, .bottomrow > child");
+board.addEventListener('click', () => {
+    console.log("hello");
+});
