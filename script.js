2c5da3b43d9a95646fef2066f7f725abf54913dd
Paulo Adeva <paulo.adeva@gmail.com>
2022-06-05 00:29:00 +1000

    added stop gameplay

--- script.js
+++ script.js
@@ -74,13 +74,11 @@ const gameBoard = () => {
                 square.innerText = "close";
                 trackPlayer.push(2);
                 square.onclick = null;
-                
             }
             else if (trackPlayer[lastIndex()] == 2) {
                 square.innerText = "circle";
                 trackPlayer.push(1);
                 square.onclick = null;
-
             };
 
             checkWin();
@@ -89,7 +87,7 @@ const gameBoard = () => {
         container.appendChild(square);
 
         i ++;
-        j++;
+        j ++;
     };
 
 };
@@ -98,55 +96,60 @@ gameBoard();
 
 const checkWin = () => {
     let square = document.querySelectorAll('.square');
+    let container = document.querySelector('.container')
     let i = 0;
 
-    let horizontal = document.querySelector('.containerHorizontal');
     let horizontalTop = document.querySelector('.horizontal.top');
     let horizontalMid = document.querySelector('.horizontal.mid');
     let horizontalBot = document.querySelector('.horizontal.bot');
 
-
-    let vertical = document.querySelector('.containerHorizontal');
     let verticalLeft = document.querySelector('.vertical.left');
     let verticalMid = document.querySelector('.vertical.mid');
     let verticalRight = document.querySelector('.vertical.right');
 
-
-    let diagonal = document.querySelector('.containerHorizontal');
     let diagonalLeft = document.querySelector('.diagonal.left');
     let diagonalRight = document.querySelector('.diagonal.right');
+
     if (square[0].innerText != "") {
         if (square[0].innerText === square[1].innerText && square[1].innerText === square[2].innerText) {
             horizontalTop.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
         if (square[0].innerText === square[4].innerText && square[4].innerText === square[8].innerText) {
             diagonalRight.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
         if (square[0].innerText === square[3].innerText && square[3].innerText === square[6].innerText) {
             verticalLeft.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
     }
     if (square[1].innerText != "") {
         if (square[1].innerText === square[4].innerText && square[4].innerText === square[7].innerText) {
             verticalMid.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
     }
     if (square[2].innerText != "") {
         if (square[2].innerText === square[5].innerText && square[5].innerText === square[8].innerText) {
             verticalRight.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
         if (square[2].innerText === square[4].innerText && square[4].innerText === square[6].innerText) {
             diagonalLeft.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
     }
     if (square[3].innerText != "") {
         if (square[3].innerText === square[4].innerText && square[4].innerText === square[5].innerText) {
             horizontalMid.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
     }
     if (square[6].innerText != "") {
         if (square[6].innerText === square[7].innerText && square[7].innerText === square[8].innerText) {
             horizontalBot.style.visibility = 'visible';
+            container.style.pointerEvents = 'none';
         }
     }
 }
