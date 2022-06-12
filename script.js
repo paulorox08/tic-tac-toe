3c385f4e6804ef6ac1b3b5b7171306e45c14e578
Paulo Adeva <paulo.adeva@gmail.com>
2022-06-12 23:24:46 +1000

    trying to add themes

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-export {trackPlayer, lastIndex, playerTurn, gameBoard, updateScore};
-
-import {checkWin} from './modules/checkWin.js';
-
-
 let trackPlayer = [1];
 
 function lastIndex() {
@@ -73,6 +68,81 @@ const updateScore = () => {
     }
 }
 
+const checkWin = () => {
+    let square = document.querySelectorAll('.square');
+    let container = document.querySelector('.container')
+
+    let horizontalTop = document.querySelector('.horizontal.top');
+    let horizontalMid = document.querySelector('.horizontal.mid');
+    let horizontalBot = document.querySelector('.horizontal.bot');
+
+    let verticalLeft = document.querySelector('.vertical.left');
+    let verticalMid = document.querySelector('.vertical.mid');
+    let verticalRight = document.querySelector('.vertical.right');
+
+    let diagonalLeft = document.querySelector('.diagonal.left');
+    let diagonalRight = document.querySelector('.diagonal.right');
+
+    if (square[0].innerText != "") {
+        if (square[0].innerText === square[1].innerText && square[1].innerText === square[2].innerText) {
+            horizontalTop.style.opacity = '1';
+            horizontalTop.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+        if (square[0].innerText === square[4].innerText && square[4].innerText === square[8].innerText) {
+            diagonalRight.style.opacity = '1';
+            diagonalRight.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+        if (square[0].innerText === square[3].innerText && square[3].innerText === square[6].innerText) {
+            verticalLeft.style.opacity = '1';
+            verticalLeft.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+    }
+    if (square[1].innerText != "") {
+        if (square[1].innerText === square[4].innerText && square[4].innerText === square[7].innerText) {
+            verticalMid.style.opacity = '1';
+            verticalMid.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+    }
+    if (square[2].innerText != "") {
+        if (square[2].innerText === square[5].innerText && square[5].innerText === square[8].innerText) {
+            verticalRight.style.opacity = '1';
+            verticalRight.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+        if (square[2].innerText === square[4].innerText && square[4].innerText === square[6].innerText) {
+            diagonalLeft.style.opacity = '1';
+            diagonalLeft.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+    }
+    if (square[3].innerText != "") {
+        if (square[3].innerText === square[4].innerText && square[4].innerText === square[5].innerText) {
+            horizontalMid.style.opacity = '1';
+            horizontalMid.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+    }
+    if (square[6].innerText != "") {
+        if (square[6].innerText === square[7].innerText && square[7].innerText === square[8].innerText) {
+            horizontalBot.style.opacity = '1';
+            horizontalBot.style.transition = '1s ease';
+            container.style.pointerEvents = 'none';
+            updateScore();
+        }
+    }
+}
+
 const newGame = () => {
     const refresh = document.querySelector('.refresh');
     const container = document.querySelector('.container');
@@ -117,3 +187,13 @@ const newGame = () => {
 }
 newGame();
 
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark_mode' ? 'light_mode' : 'dark_mode';
+    root.className = newTheme;
+    
+    document.querySelector('.toggle').innerText = newTheme;
+  }
+  
+  setTheme();
+  document.querySelector('.toggle').addEventListener('click', setTheme);
